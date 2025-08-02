15efce68d99edbacdd0e319401d01edc42c0e122
thanhnm3 <minhthanh20032006@gmail.com>
2025-08-02 08:05:36 +0700

    update btn delete category

--- products/urls.py
+++ products/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     # Categories
     path('categories/', views.category_list, name='category_list'),
     path('categories/create/', views.category_create, name='category_create'),
+    path('categories/<int:pk>/delete/', views.category_delete, name='category_delete'),
 ] 
